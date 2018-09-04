debd1f968f6a189db3c9a0a8294fea5bc4129313
Sandeep Singh <soami.ssr@gmail.com>
2018-09-04 09:46:44 +0530

    updated

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,32 +6,32 @@ import { connect } from 'react-redux';
 
 const data = [
   {
-    Name: 'test1',
+    name: 'test1',
     age: 26,
     email: 'test@test.com',
   },
   {
-    Name: 'test2',
+    name: 'test2',
     age: 26,
     email: 'test2@test.com',
   },
   {
-    Name: 'test3',
+    name: 'test3',
     age: 26,
     email: 'test3@test.com',
   },
   {
-    Name: 'test4',
+    name: 'test4',
     age: 26,
     email: 'test4@test.com',
   },
   {
-    Name: 'test5',
+    name: 'test5',
     age: 26,
     email: 'test5@test.com',
   },
   {
-    Name: 'test6',
+    name: 'test6',
     age: 26,
     email: 'test6@test.com',
   },
@@ -41,7 +41,8 @@ class App extends Component {
   state = {
     data: {
       headers: [],
-      values: [...data],
+      headersTxt: [],
+      values: data,
       item: {
         name: '',
         age: '',
@@ -54,12 +55,27 @@ class App extends Component {
     const { item = {} } = this.state.data;
     let items = Object.keys(item);
     let arr = items && items.map( item => ({ [item]: true }));
-    this.setState({ data: { ...data, headers: arr }})
+    this.setState({
+      data: {
+        ...this.state.data,
+        headers: arr,
+        headersTxt: items
+      }
+    })
+  }
+
+  onChange = (key, header) => {
+    let { data } = this.state;
+    let { headers } = data;
+    headers[key][header] = !headers[key][header]
+    this.setState({ data })
   }
 
   render() {
     const { data = [] } = this.state;
-    const { headers = [], values = []} = data;
+    const { headers = [], values = [], headersTxt = [] } = data;
+    console.log(values, 'values');
+    console.log(data, 'data');
 
     return (
       <Grid container spacing={0}>
@@ -67,17 +83,18 @@ class App extends Component {
           <Grid item xs={12}>
             <h4>Show/Hide Columns</h4>
           </Grid>
-          {headers && headers.map((header, key) => <Grid container key={key}>
+          {headersTxt && headersTxt.map((header, key) => <Grid container key={key}>
             <Grid item xs={12}>
+            {console.log(headers[key][header], headers, header)}
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked=""
-                    onChange=""
-                    value="checkedA"
+                    checked={headers[key][header]}
+                    onChange={this.onChange.bind(key, header)}
+                    value={headers[header]}
                   />
                 }
-                label={Object.getOwnPropertyNames(header)}
+                label={header}
               />
             </Grid>
           </Grid>)}
@@ -97,11 +114,12 @@ class App extends Component {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  <TableRow>
-                    <TableCell>1</TableCell>
-                    <TableCell>2</TableCell>
-                    <TableCell>3</TableCell>
-                  </TableRow>
+                  {
+                    values && values.map((item, i) => <TableRow key={i}>
+                      
+                      {headers && headers.map((value, key) => <TableCell key={key}>{item[Object.getOwnPropertyNames(value)]}</TableCell>)}
+                    </TableRow>)
+                  }
                 </TableBody>
               </Table>
             </Grid>
